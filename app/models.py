7ddbaab197673b8e6041218cf6ee1c824abc6727
Dibek1910 <kooldibek138a@gmail.com>
2024-12-22 00:13:12 +0530

    Updated customer4

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Date, Float, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Float, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from app.database import Base
 from datetime import datetime
