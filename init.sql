d98ccf8b2cda13f95c5e049b04d5331b6b0befa8
Dibek1910 <kooldibek138a@gmail.com>
2024-12-28 13:18:53 +0530

    Updated

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE customers (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     middle_name VARCHAR(50),
     last_name VARCHAR(50) NOT NULL,
@@ -23,11 +23,11 @@ CREATE TABLE customers (
 );
 
 CREATE TABLE accounts (
-    id SERIAL PRIMARY KEY,
-    customer_id INTEGER REFERENCES customers(id) NOT NULL,
+    id INTEGER PRIMARY KEY,
+    customer_id BIGINT REFERENCES customers(id) NOT NULL,
     account_type VARCHAR(20) NOT NULL,
     balance NUMERIC(15, 2) DEFAULT 0.0,
-    currency VARCHAR(3) DEFAULT 'USD',
+    currency VARCHAR(3) DEFAULT 'INR',
     status VARCHAR(20) DEFAULT 'Active',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
@@ -35,10 +35,12 @@ CREATE TABLE accounts (
 
 CREATE TABLE documents (
     id SERIAL PRIMARY KEY,
-    customer_id INTEGER REFERENCES customers(id) NOT NULL,
+    customer_id BIGINT REFERENCES customers(id) NOT NULL,
     document_type VARCHAR(50) NOT NULL,
     document_path VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
+
+ALTER TABLE customers ADD CONSTRAINT fk_primary_account FOREIGN KEY (primary_account_id) REFERENCES accounts(id);
 
